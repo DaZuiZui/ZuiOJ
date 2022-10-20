e39a759e4640f3dd8ca31c39916d1fe2b35d2f87
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2022-10-20 22:30:11 +0800

    api
    api

---
+++ ZuiOJ/basic-api/src/main/java/com/dazuizui/basicapi/BasicApiApplication.java
@@ -0,0 +1,13 @@
+package com.dazuizui.basicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicApiApplication.class, args);
+    }
+
+}
