0c9f61344b5e62ceb22dc04209ac3a5f5e5936bd
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2022-10-20 21:52:27 +0800

    Add files via upload
    基础文件

---
+++ ZuiOJ/basic-demo/online_judge/src/main/java/com/dazuizui/online_judge/OnlineJudgeApplication.java
@@ -0,0 +1,13 @@
+package com.dazuizui.online_judge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineJudgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineJudgeApplication.class, args);
+    }
+
+}
---
+++ ZuiOJ/basic-demo/online_judge/src/test/java/com/dazuizui/online_judge/OnlineJudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazuizui.online_judge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineJudgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
