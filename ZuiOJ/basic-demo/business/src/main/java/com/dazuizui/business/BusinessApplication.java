71e9f33b6389cb6fcc83e2687312e8c0561881ec
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2022-10-22 21:09:08 +0800

    Add files via upload
    业务实现类

---
+++ ZuiOJ/basic-demo/business/src/main/java/com/dazuizui/business/BusinessApplication.java
@@ -0,0 +1,13 @@
+package com.dazuizui.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApplication.class, args);
+    }
+
+}
---
+++ ZuiOJ/basic-demo/business/src/test/java/com/dazuizui/business/BusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazuizui.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
