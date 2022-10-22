58ab7cc2c0c5285f4ff29f70d41b71ee808f5c4b
DaZuiZui <y51288033@outlook.com>
2022-10-22 21:28:19 +0800

    业务类初始化

--- ZuiOJ/basic-demo/business/src/main/java/com/dazuizui/business/BusinessApplication.java
+++ ZuiOJ/basic-demo/business/src/main/java/com/dazuizui/business/BusinessApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BusinessApplication {
-
     public static void main(String[] args) {
+        System.out.println("Welcome to ZuiOJ - TLMOJ in 哈尔滨广厦学院校区");
+        System.out.println("Hi This is TLM Team, we are form 哈尔滨广厦学院");
         SpringApplication.run(BusinessApplication.class, args);
+        System.out.println("ZuiOJ - TLMOJ starting");
     }
-
 }
