5c7bbe89cffe4a3768877d41a5246b23e5837eb0
“DaZuiZui” <y51288033@outlook.com>
2022-10-25 21:56:43 +0800

    登入

--- ZuiOJWeb/src/router/index.js
+++ ZuiOJWeb/src/router/index.js
@@ -2,15 +2,24 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Axios from 'axios';
+import Login from '@/components/view/Login'
+
 Vue.use(Router)
 Vue.prototype.$axios = Axios;
 export default new Router({
+  mode: 'history',
+  base: '/h',
   //root
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    //登入
+    {
+      path: '/user/login',
+      component: Login
     }
   ]
 })
