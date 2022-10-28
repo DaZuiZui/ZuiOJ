9a4741dc3e3942b6c472696142448d0143b07dde
DaZuiZui <y51288033@outlook.com>
2022-10-29 02:08:37 +0800

    前端优化V1.1.0

--- ZuiOJWeb/static/request.js
+++ ZuiOJWeb/static/request.js
@@ -1,2 +1,14 @@
 var url = "http://127.0.0.1";
 var port = 8001;
+
+
+import Axios from 'axios'
+export async function  synRequest(controller,data) {
+    var arr = '';
+    await Axios.post("http://127.0.0.1:8001/"+controller,data)
+      .then(res =>{
+            arr = res.data;
+        })
+
+       return arr;
+}
