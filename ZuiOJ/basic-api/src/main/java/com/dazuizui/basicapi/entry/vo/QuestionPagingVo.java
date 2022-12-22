7c5de1de3145a2b180ed79fd4785d82dafcc709e
“DaZuiZui” <y51288033@outlook.com>
2022-12-22 18:34:27 +0800

    适配新系统

--- ZuiOJ/basic-api/src/main/java/com/dazuizui/basicapi/entry/vo/QuestionPagingVo.java
+++ ZuiOJ/basic-api/src/main/java/com/dazuizui/basicapi/entry/vo/QuestionPagingVo.java
@@ -1,2 +1,48 @@
-package com.dazuizui.basicapi.entry.vo;public class PagingVo {
+package com.dazuizui.basicapi.entry.vo;
+
+import com.dazuizui.basicapi.entry.QuestionBank;
+
+import java.util.List;
+
+/**
+ * 分页查询Vo
+ */
+public class QuestionPagingVo {
+    //分页查询的数据
+    private List<QuestionBank> questionBanks;
+    //总数据量
+    private Long countOfQuestion;
+
+    @Override
+    public String toString() {
+        return "QuestionPagingVo{" +
+                "questionBanks=" + questionBanks +
+                ", countOfQuestion=" + countOfQuestion +
+                '}';
+    }
+
+    public List<QuestionBank> getQuestionBanks() {
+        return questionBanks;
+    }
+
+    public void setQuestionBanks(List<QuestionBank> questionBanks) {
+        this.questionBanks = questionBanks;
+    }
+
+    public Long getCountOfQuestion() {
+        return countOfQuestion;
+    }
+
+    public void setCountOfQuestion(Long countOfQuestion) {
+        this.countOfQuestion = countOfQuestion;
+    }
+
+    public QuestionPagingVo(List<QuestionBank> questionBanks) {
+        this.questionBanks = questionBanks;
+    }
+
+    public QuestionPagingVo(List<QuestionBank> questionBanks, Long countOfQuestion) {
+        this.questionBanks = questionBanks;
+        this.countOfQuestion = countOfQuestion;
+    }
 }
---
+++ ZuiOJWeb v2.0/src/components/view/main/cn/question/beifen.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="hello">
+        <div>
+            <Top></Top>
+        </div>
+        <!-- Main content -->
+  
+        <section class="slice slice-lg pt-lg-6 pb-0 pb-lg-6 bg-section-secondary">
+            <div class="container">
+                <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col" width="250px">#</th>
+                        <th scope="col">编号</th>
+                        <th scope="col">题目</th>
+                        <th scope="col">难度</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>等待统计</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  
+                  <!--分页部分-->
+                  <el-pagination
+                    :page-size="50"
+                    :pager-count="11"
+                    @current-change="getMerchantInformation"
+                    layout="prev, pager, next"
+                    :total="1000">
+                  </el-pagination>
+            </div>
+        </section>
+ 
+        <footer class="position-relative" id="footer-main">
+            <Foot></Foot>
+        </footer>
+    </div>
+  </template>
+  
+  <script>
+  import Foot from '../../../../frame/blog/Foot.vue';
+  import Top  from '../../../../frame/blog/Top.vue'
+  export default {
+    name: 'HelloWorld',
+    components: {
+           Foot,Top
+      },
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    methods: {
+        //跳转指定页面
+        getMerchantInformation(val){   
+            alert(val);
+        },
+        
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  </style>
+  
