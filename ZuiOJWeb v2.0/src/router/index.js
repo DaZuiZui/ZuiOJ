e6528c9dc09571a29337504be72655fd4636b443
“DaZuiZui” <y51288033@outlook.com>
2022-12-30 20:47:43 +0800

    文件上传

--- ZuiOJWeb v2.0/src/router/index.js
+++ ZuiOJWeb v2.0/src/router/index.js
@@ -17,6 +17,7 @@ import AdminQuestionList from '@/components/view/main/cn/admin/AdminQuestionList
 import AdminQuestioncase from '@/components/view/main/cn/admin/AdminQuestionCase'
 import AdminUserList from '@/components/view/main/cn/admin/AdminuserList'
 import Register from '@/components/view/main/cn/user/Register'
+import UserInfo from '@/components/view/main/cn/user/UserInfo'
 Vue.use(Router)
 
 export default new Router({
@@ -27,6 +28,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: AboutDemo
+    },
+    //中文用户信息
+    {
+      path: '/user/info',
+      name: 'UserInfo',
+      component: UserInfo
     },
     //中文用户注册
     {
