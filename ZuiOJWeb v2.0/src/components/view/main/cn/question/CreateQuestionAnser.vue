8c9a8132f0b58dd5cf2d696c47ab959ae1c54bf7
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-01-25 22:45:04 +0800

    test

---
+++ ZuiOJWeb v2.0/src/components/view/main/cn/question/CreateQuestionAnser.vue
@@ -0,0 +1,291 @@
+<template>
+  <div class="hello">
+      <div>
+          <Top></Top>
+      </div>
+
+      <!-- Main content -->
+      <section  style="background-color:#f9f9f9">
+          <div class="row mb-5 justify-content-center text-center" >
+              <div class="col-lg-9"  >
+                 
+                  <h2 class=" mt-4"> 创建题解</h2>
+                  <div class="mt-2">
+                      <p class="lead lh-180"> Create Question Anser </p>
+                  </div>
+                         
+                  <div>
+                      <div style="float:left;margin-top:6px">
+                          标题: &nbsp;
+                      </div> 
+                      <div style="float:left;width:93%">
+                          <el-input v-model="article.title" placeholder="请输入标题"  maxlength="50"></el-input>
+                      </div>
+                      <br><br>
+                      <div style="float:left;margin-top:6px">
+                          简介: &nbsp;
+                      </div> 
+                 
+                      <div style="float:left;width:93%">
+                       <el-input v-model="article.introduce" placeholder="请输入介绍" maxlength="100"></el-input>
+                      </div>
+                      <br><br>
+                     
+                      
+                          <div style="float:left;margin-top:6px">
+                              权限: &nbsp;
+                          </div> 
+                          <div style="float:left;">
+                              <el-select v-model="article.privacy" placeholder="请选择">
+                                  <el-option value="0" key="0" label="Public"></el-option>
+                                  <el-option value="1" key="1" label="Private"></el-option>
+                              </el-select>
+                          </div>
+
+                          <div style="float:left;margin-top:5px;margin-left:20px">
+                              分类: &nbsp;
+                          </div> 
+
+                        
+                          <div style="float:left;">
+                              <el-select  
+                                  v-model="article.articleTypeArray"
+                                  multiple
+                                  collapse-tags
+                                  style="margin-left: 20px;width:210px"
+                                  placeholder="请选择">
+                                  <el-option
+                                      v-for="item in articleType"
+                                      :key="item.value"
+                                      :label="item.label"
+                                      :value="item.value">
+                                  </el-option>
+                              </el-select>
+                          </div>
+
+                          <div style="float:left;margin-top:5px;margin-left:20px">
+                              语言: &nbsp;
+                          </div> 
+                          <div style="float:left;">
+                              <el-select  
+                                  v-model="article.languageTypeArray"
+                                  multiple
+                                  collapse-tags
+                                  style="margin-left: 20px;width:150px;"
+                                  placeholder="请选择">
+                                  <el-option
+                                      v-for="item in languageType"
+                                      :key="item.value"
+                                      :label="item.label"
+                                      :value="item.value">
+                                  </el-option>
+                              </el-select>
+                          </div>
+                          
+                          <div style="float:left;margin-top:5px;margin-left:20px">
+                              技术: &nbsp;
+                          </div> 
+                          <div style="float:left;">
+                              <el-select v-model="article.technologyType" placeholder="请选择">
+                                  <el-option value="0" value-key="0" key="0" label="问答">问答</el-option>
+                                  <el-option value="1" value-key="1" key="1" label="后端">后端</el-option>
+                                  <el-option value="2" value-key="2" key="2" label="前端">前端</el-option>
+                                  <el-option value="3" value-key="3" key="3" label="架构">架构</el-option>
+                                  <el-option value="4" value-key="4" key="4" label="安全">安全</el-option>
+                                  <el-option value="5" value-key="5" key="5" label="数据库">数据库</el-option>
+                                  <el-option value="6" value-key="6" key="6" label="学习日记"></el-option>
+                                  <el-option value="7" value-key="7" key="7" label="踩坑记录">踩坑记录</el-option>
+                                  <el-option value="8" value-key="8" key="8" label="算法问题"></el-option>
+                                </el-select>
+                          </div>
+
+                          <br><br>
+                          <div style="float:left;margin-top:5px;">
+                              我的文件夹分类: &nbsp;
+                          </div> 
+                          <div style="float:left;">
+                              <el-select
+                                  v-model="article.myFileList"
+                                  multiple
+                                  filterable
+                                  allow-create
+                                  default-first-option
+                                  placeholder="请选择文章标签">
+                              
+                                  <el-option
+                                  
+                                  v-for="item in myFilesList"
+                                      :key="item.value"
+                                      :label="item.label"
+                                      :value="item.value">
+                                  </el-option>
+                              </el-select>
+                          </div>
+
+                          <div style="float:right;margin-right:30px">
+                              <button type="button" class="btn btn-success">保存</button>
+                              <button type="button" class="btn btn-primary" @click="submit">提交</button>
+                          </div>
+                          
+                          <div style="clear:both">
+                       
+                              <br>
+                          </div>
+                        
+                           <!--编辑器区域-->
+                           <div  style=" width:100%;clear:both"> 
+                              <mavon-editor  
+                                  codeStyle=""   
+                                  placeholder="请输入您的文章"
+                                  v-model="article.mdText"
+                                  @change="change"
+                              />
+                          </div>
+                  </div>
+                  
+              </div>
+       
+          </div>
+      </section>
+
+      <footer class="position-relative" id="footer-main">
+          <Foot></Foot>
+      </footer>
+  </div>
+</template>
+
+
+<script>
+import { synRequestGet, synRequestPost } from '../../../../../../static/request';
+import Foot from '../../../../frame/blog/Foot.vue';
+import Top  from '../../../../frame/blog/Top.vue'
+export default {
+  name: 'HelloWorld',
+  components: {
+         Foot,Top
+    },
+  data () {
+    return {
+      //讨论分类
+      articleType: [{
+        value: 1,
+        label: '校园交流'
+      }, {
+        value: 2,
+        label: '求职交流'
+      }, {
+        value: '3',
+        label: '信息学院技术交流'
+      }, {
+        value: '4',
+        label: '信息学院专业社团'
+      },],
+
+      //讨论分类
+      articleType: [{
+        value: '3',
+        label: '信息学院技术交流'
+      }, {
+        value: '4',
+        label: '信息学院专业社团'
+      },],
+
+      //语言分类
+      languageType: [{
+        value: 1,
+        label: 'Java'
+      }, {
+        value: 2,
+        label: 'C/C++'
+      }, {
+        value: '3',
+        label: 'Python'
+      }, {
+        value: '4',
+        label: 'Vue'
+      },{
+        value: '5',
+        label: 'JS'
+      },],
+
+      //我的文件夹分类
+      myFilesList: null,
+
+      article: {
+          privacy: "0",     //权限
+          articleType: null, //文章分类
+          language: null,    //语言id
+          technologyType: "6", //技术分类
+          //分类讨论
+          articleTypeArray: [],
+          //语言分类
+          languageTypeArray: [],
+          //我的文件夹分类
+          myFileList: [],
+          //Md文档
+          mdText: null,
+          //标题
+          title: "",
+          //介绍
+          introduce: "",
+          //token
+          token: "",
+          //幂等性token
+          nonPowerToken: "",
+      }
+    }
+  },
+  methods: {
+      //跳转指定页面
+      getMerchantInformation(val){   
+          alert(val);
+      },
+      
+      change(value, render) {
+          this.article.mdText = value;
+      },
+
+      //防止幂等性
+      async getNonPowerToken(){
+          var object = await synRequestGet("/system/getNonPowerTokenString");
+          this.article.nonPowerToken = object.data;
+      },
+
+      //提交
+      async submit(){
+          console.log(this.article);
+          var object = await synRequestPost("/blog/createQuestionAnswer?questionId="+getQueryVariable("id"),this.article);
+          if(check(object) == true){
+             alert("添加成功");
+          }
+      }
+  },
+  //自启动
+  mounted() {
+      //获取token
+      this.article.token = getCookie("token");
+      //获取幂等性token
+      this.article.nonPowerToken = this.getNonPowerToken();
+  },
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+</style>
---
+++ ZuiOJWeb v2.0/src/components/view/main/cn/question/QuestionAnser.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="hello">
+        <div>
+            <Top></Top>
+        </div>
+
+        <!-- Main content -->
+        <section  style="background-color:#f9f9f9">
+            <br>
+            <div class="text-center" style="1000px" >
+                <div class="row mb-5 justify-content-center text-center" >
+                    <div class="col-lg-9"  >
+
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                              <li class="breadcrumb-item" aria-current="page">  <el-link :underline="false">Back</el-link></li>
+                              <li class="breadcrumb-item"><a href="#">题解</a></li>
+                              <li class="breadcrumb-item active" aria-current="page"><el-link :underline="false">评论</el-link></li>
+                              <li class="breadcrumb-item active" aria-current="page"><el-link :underline="false">贡献此题案例</el-link></li>
+                              <li class="breadcrumb-item active" aria-current="page" @click="goCreateAnser()">
+                                <el-link :underline="false" >贡献此题题解</el-link>
+                            </li>
+                              <li class="breadcrumb-item active" aria-current="page"><el-link :underline="false">此题贡献者团队</el-link></li>
+                            </ol>
+                        </nav>
+                        
+                        <br>
+
+                        <div style="width:100%">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div style="float:left">
+                                        <h4>贪心算法 
+                                            <span class="badge badge-secondary">Java</span>
+                                            <span class="badge badge-secondary">C++</span>
+                                            <span class="badge badge-secondary">C/C++</span>
+                                        </h4>
+                                    </div>
+                                    <div style="both:clear">
+                                        <br>
+                                        <hr>
+                                    </div>
+                                    <div style="float:left;margin-left:40px">
+                                        asd
+                                    </div>
+
+                                    <div style="both:clear">
+                                        <br>
+                                        <hr>
+                                    </div>
+
+                                    <div style="float:right">
+                                        题解贡献者: Bryan
+                                    </div>
+                                </div>
+                              </div>   
+                            </div>
+                        <br>
+                    </div>
+                </div>    
+              &nbsp;
+             
+            </div>
+        </section>
+ 
+        <footer class="position-relative" id="footer-main">
+            <Foot></Foot>
+        </footer>
+    </div>
+  </template>
+  
+  <script>
+  import Foot from '../../../../frame/blog/Foot.vue';
+  import Top  from '../../../../frame/blog/Top.vue'
+  export default {
+    name: 'HelloWorld',
+    components: {
+           Foot,Top
+      },
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    methods: {
+        //跳转指定页面
+        getMerchantInformation(val){   
+            alert(val);
+        },
+        //创建题解
+        goCreateAnser(){
+            alert("Asd");
+            //window.location.href='/cn/question/createAnser?id='+getQueryVariable("id")
+            this.$router.push('/cn/question/CreateQuestionAnwer?id='+getQueryVariable("id"));
+        },
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  </style>
+  
