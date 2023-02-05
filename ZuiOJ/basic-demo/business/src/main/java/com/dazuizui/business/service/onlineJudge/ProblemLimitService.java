4e2d5ce58aa049721f6ed9c710b5c19ad2776d62
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-02-05 23:40:35 +0800

    自定义算法题限制优化

---
+++ ZuiOJ/basic-demo/business/src/main/java/com/dazuizui/business/service/onlineJudge/ProblemLimitService.java
@@ -0,0 +1,2 @@
+package com.dazuizui.business.service.onlineJudge;public interface ProblemLimitService {
+}
---
+++ ZuiOJ/basic-demo/business/src/main/java/com/dazuizui/business/service/onlineJudge/impl/ProblemLimitServiceImpl.java
@@ -0,0 +1,2 @@
+package com.dazuizui.business.service.onlineJudge.impl;public class ProblemLimitServiceImpl {
+}
