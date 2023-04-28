47dbc54aae7bf907f363b38b2efb16f7d1f50f11
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-04-28 22:42:18 +0800

    封装监考人员鉴权

--- ZuiOJ/basic-api/src/main/java/com/dazuizui/basicapi/mapper/Main4.java
+++ ZuiOJ/basic-api/src/main/java/com/dazuizui/basicapi/mapper/Main4.java
@@ -4,21 +4,6 @@ import java.util.Scanner;
 
 public class Main4 {
     public static void main(String[] args) {
-      //  System.out.println("Hello world");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(sumOfNumberAndReverse(scanner.nextInt()));
+        System.out.println("Hello world");
     }
-
-
-    public static boolean sumOfNumberAndReverse(int num) {
-        for (int i = 0; i <= num ; i++) {
-            String s = new StringBuilder(i+"").reverse().toString();
-            if ((i + Integer.valueOf(s)) == num){
-                return true;
-            }
-        }
-
-        return false;
-    }
-
 }
