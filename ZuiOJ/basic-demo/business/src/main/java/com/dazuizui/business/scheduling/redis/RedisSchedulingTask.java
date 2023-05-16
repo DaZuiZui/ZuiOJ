0a0b805f3e5eb58fb12b42d4449f41a8035cc35a
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-05-16 18:56:00 +0800

    Fix the bug that the administrator cannot get the article

--- ZuiOJ/basic-demo/business/src/main/java/com/dazuizui/business/scheduling/redis/RedisSchedulingTask.java
+++ ZuiOJ/basic-demo/business/src/main/java/com/dazuizui/business/scheduling/redis/RedisSchedulingTask.java
@@ -9,44 +9,42 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-
-import java.io.Closeable;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
+
+/**
+ * @author Dazui
+ * Redis定时任务
+ */
 @Configuration
 @EnableScheduling
 public class RedisSchedulingTask {
 
     @Autowired
-    private RedisTemplate redisTemplate;
-    String cursor = "0";
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
 
     /**
+     * 每天凌晨1点执行一次任务
      * 我们通过 RedisTemplate 获取 Redis 连接工厂 RedisConnectionFactory，然后获取 Redis 连接 RedisConnection。接下来，我们使用 scan 方法进行遍历，以获取所有键。
-     *
      * 对于每个键，我们使用 ttl 方法获取其剩余生存时间（TTL），如果剩余生存时间小于等于 0，表示键已过期，我们使用 RedisTemplate 的 delete 方法删除该键。
-     *
      * 请注意，代码中的 scanOptions().match("*").count(100) 可以根据需要进行调整。match("*") 表示匹配所有键，count(100) 表示每次遍历返回的键的数量。你可以根据实际情况进行适当的调整。
      */
-    @Scheduled(fixedRate = 60000)
-    public void deleteExpiredKeys() {
-        RedisConnectionFactory connectionFactory = redisTemplate.getConnectionFactory();
-        RedisConnection connection = connectionFactory.getConnection();
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void cleanupExpiredKeys() throws IOException {
+        RedisConnection connection = redisConnectionFactory.getConnection();
 
         try (Cursor<byte[]> cursor = connection.scan(ScanOptions.scanOptions().match("*").count(100).build())) {
             while (cursor.hasNext()) {
                 byte[] keyBytes = cursor.next();
-                String key = new String(keyBytes, StandardCharsets.UTF_8);
+                String key = new String(keyBytes);
 
                 if (connection.ttl(keyBytes) <= 0) {
                     redisTemplate.delete(key);
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
