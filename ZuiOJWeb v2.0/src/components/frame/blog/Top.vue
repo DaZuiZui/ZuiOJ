15ccc00bd85575466f15ca0d7f7017deac375477
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-07-14 22:08:59 +0800

    大学主页完善

--- ZuiOJWeb v2.0/src/components/frame/blog/Top.vue
+++ ZuiOJWeb v2.0/src/components/frame/blog/Top.vue
@@ -16,7 +16,7 @@
             </div>
         </div>
         <a   class="btn btn-block btn-dark text-truncate rounded-0 py-2 d-none d-lg-block" style="z-index: 1000;color:aliceblue" target="_blank">
-            <strong>Dazui的博客</strong>  @twitter.com/Dazui0225
+            <strong>Dazui的博客</strong>  @twitter.com/Dazui0225 ZuiOJ测试版本
         </a>
 
         <div>
