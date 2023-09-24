38a696a6cbbe7a2abf82989db10ace13c186a45d
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-09-24 23:42:24 +0800

    fix #13

--- ZuiOJ/basic-demo/business/src/main/java/com/dazuizui/business/service/onlineJudge/impl/CompetitionQuestionBankSerivceImpl.java
+++ ZuiOJ/basic-demo/business/src/main/java/com/dazuizui/business/service/onlineJudge/impl/CompetitionQuestionBankSerivceImpl.java
@@ -2,16 +2,19 @@ package com.dazuizui.business.service.onlineJudge.impl;
 
 import com.alibaba.fastjson2.JSONArray;
 import com.dazuizui.basicapi.entry.CompetitionQuestionBank;
+import com.dazuizui.basicapi.entry.Contest;
 import com.dazuizui.basicapi.entry.StatusCode;
 import com.dazuizui.basicapi.entry.StatusCodeMessage;
 import com.dazuizui.basicapi.entry.vo.ContestQuestionVo;
 import com.dazuizui.basicapi.entry.vo.ResponseVo;
 import com.dazuizui.business.mapper.CompetitionQuestionBankMapper;
+import com.dazuizui.business.mapper.ContestMapper;
 import com.dazuizui.business.service.onlineJudge.CompetitionQuestionBankService;
 import com.dazuizui.business.util.ThreadLocalUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -21,6 +24,8 @@ import java.util.List;
 public class CompetitionQuestionBankSerivceImpl implements CompetitionQuestionBankService {
     @Autowired
     private CompetitionQuestionBankMapper competitionQuestionBankMapper;
+    @Autowired
+    private ContestMapper contestMapper;
 
     /**
      * 添加比赛关联题目
@@ -68,9 +73,17 @@ public class CompetitionQuestionBankSerivceImpl implements CompetitionQuestionBa
         //获取比赛人ID
         String idstring = (String) ThreadLocalUtil.mapThreadLocalOfJWT.get().get("userinfo").get("id");
 
+        //查看当前时间是否可以获取比赛
+        Contest eventById = contestMapper.getEventById(contestId);
+        Date date = new Date();
+        if (date.compareTo(eventById.getStartTime()) < 0){
+            return JSONArray.toJSONString(new ResponseVo<>("未到达获取题目时间",null,"0x945"));
+        }
+
+
         //获取题库
         List<ContestQuestionVo> questionListInContest = competitionQuestionBankMapper.getQuestionListInContest(contestId,Long.valueOf(idstring));
-        System.err.println(questionListInContest);
+
 
 
         return JSONArray.toJSONString(new ResponseVo<>("获取比赛中的题库",questionListInContest,"200"));
